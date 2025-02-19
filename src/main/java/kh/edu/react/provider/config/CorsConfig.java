fc4883705ef0bef0fe16088da8ed12e0e23c8e77
coiilee <dakyung96@gmail.com>
2025-02-19 10:48:54 +0900

    Initial commit

---
+++ src/main/java/kh/edu/react/provider/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package kh.edu.react.provider.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// 리액트 3000 포트와 연결을 주고 받을 때 어디까지 react 가 sprinboot  에서
+// 제공되는 값을 사용할 수 있는가? 설정
+// !!! Controller 에서 @CrossOrigin 이랑 같이 사용 XXX
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                      .allowedOrigins("http://localhost:3000")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
